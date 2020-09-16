ec0c011bf4681d1b7f7db276d74ab44eec289680
Paul Martens <paul.martens@divotion.com>
2020-09-17 00:33:58 +0200

    Set up basic styling and page layout

---
+++ components/layout/Page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page: React.FC = (props) => (
+	<main className="Page">
+		{props.children}
+	</main>
+);
+
+export default Page;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import Page from 'components/layout/Page';
+
 import 'styles/app.scss';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-	<React.Fragment>
+	<Page>
 		<Head>
 			<meta charSet="utf-8"/>
 			<title>FFXIV Help</title>
 			<link rel="icon" href="/favicon.png" />
+			<link rel="stylesheet" href="//fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" />
 		</Head>
 
 		<Component {...pageProps}/>
-	</React.Fragment>
+	</Page>
 );
 
 export default App;
